beb6849441983f03439f9cf03c862a6a01b6db59
Ryabkov Igor <i.ryabkov@mon-techsup.one-infra.ru>
2025-10-17 02:34:06 +0300

    Rewrite tests

---
+++ hw-01/test_task1.py
@@ -0,0 +1,11 @@
+from task1 import predict_message_mood
+
+def test_predict_specific_cases():
+    assert predict_message_mood("Чапаев и пустота") == "отл"
+    assert predict_message_mood("Чапаев и пустота", 0.8, 0.99) == "норм"
+    assert predict_message_mood("Вулкан") == "неуд"
+
+def test_predict_general_cases():
+    assert predict_message_mood("aabb") == "норм"
+    assert predict_message_mood("aaaaaa") == "неуд"
+    assert predict_message_mood("abcdef") == "отл"
